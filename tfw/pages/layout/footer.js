24ba9fbd6265c0a15cd86c6df248aebdf0225050
yangxiaol <424310717@qq.com>
2017-07-13 09:19:11 +0800

    tfw

---
+++ tfw/pages/layout/footer.js
@@ -0,0 +1,12 @@
+// pages/layout/footer.js
+Page({
+  data: {
+    iconSize: [20],
+    iconColor: [
+      'red', 'orange', 'yellow'
+    ],
+    iconType: [
+      'success', 'success_no_circle', 'info'
+    ]
+  }
+})
